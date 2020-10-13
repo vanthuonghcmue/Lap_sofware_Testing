72fd8286931612e33a1cc25c25c20c0491f30a8e
vanthuonghcmue <47852060+vanthuonghcmue@users.noreply.github.com>
2020-10-13 15:31:13 +0700

    lap 6 7

--- Scripts/testcase2/Script1601314084401.groovy
+++ Scripts/testcase2/Script1601314084401.groovy
@@ -14,6 +14,22 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser(GlobalVariable.URL)
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/a_Make Appointment'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/div_Demo account_input-group'))
+
+WebUI.setText(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Username_username'), 'ThisIsNotAPassword')
+
+WebUI.verifyElementText(findTestObject('Object Repository/Page_CURA Healthcare Service/div_Demo account_input-group_1'), 
+    'Login')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Password_password'), 'g3/DOGG74jC3Flrr3yH+3D/yKbOqqUNM')
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/button_Login'))
 
---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c7578b6c-fd08-477b-9386-de98250afbe8</testSuiteGuid>
+   <testCaseLink>
+      <guid>affde189-79aa-4791-a3ff-3cabf27247b1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testcase1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4399f8ec-983f-464a-b865-7acc8e49abe8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testcase2</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>bd8520c3-92c1-445d-a25f-0e117326ffd0</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>73c8afd8-08cb-49ab-a691-786378e4e65b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testcase3</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>7ce43907-b76f-4ea5-9de5-fd8101bc4a30</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>79a1f865-4180-41e1-98bc-e570e33f70cf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testcase4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e6f14f47-ade8-4a9b-8dce-4ec468c4d1ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testcase5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
